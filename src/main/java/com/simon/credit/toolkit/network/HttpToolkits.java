3ae3b3fbacfe15097e4515ae8ef1616b0155d4f4
xuziming <xuzimin_g@163.com>
2019-06-30 18:35:09 +0800

    improve HTTP toolkits

---
+++ src/main/java/com/simon/credit/toolkit/network/HttpToolkits.java
@@ -0,0 +1,490 @@
+package com.simon.credit.toolkit.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import com.simon.credit.toolkit.common.CommonToolkits;
+
+/**
+ * HTTP工具
+ */
+public class HttpToolkits {
+
+    private static final String     DEFAULT_CHARSET = "UTF8";
+    private static final String     METHOD_POST     = "POST";
+    private static final String     METHOD_GET      = "GET";
+
+    private static SSLContext       SSL_CONTEXT     = null;
+
+    private static HostnameVerifier VERIFIER        = null;
+
+    private static SSLSocketFactory SOCKET_FACTORY  = null;
+
+    static {
+
+        try {
+            SSL_CONTEXT = SSLContext.getInstance("TLS");
+            SSL_CONTEXT.init(new KeyManager[0], new TrustManager[] { new X509TrustManager() {
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] arg0,
+                                               String arg1) throws CertificateException {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] arg0,
+                                               String arg1) throws CertificateException {
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            } }, new SecureRandom());
+
+            SSL_CONTEXT.getClientSessionContext().setSessionTimeout(15);
+            SSL_CONTEXT.getClientSessionContext().setSessionCacheSize(1000);
+
+            SOCKET_FACTORY = SSL_CONTEXT.getSocketFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalStateException(e);
+        }
+
+        VERIFIER = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+
+    }
+
+    private HttpToolkits() {
+    }
+
+    /**
+     * 执行HTTP POST请求。
+     * 
+     * @param url 请求地址
+     * @param params 请求参数
+     * @return 响应字符串
+     * @throws IOException
+     */
+    public static String doPost(String url, Map<String, String> params, int connectTimeout,
+                                int readTimeout) throws IOException {
+        return doPost(url, params, DEFAULT_CHARSET, connectTimeout, readTimeout);
+    }
+
+    /**
+     * 执行HTTP POST请求。
+     * 
+     * @param url 请求地址
+     * @param params 请求参数
+     * @param charset 字符集，如UTF-8, GBK, GB2312
+     * @return 响应字符串
+     * @throws IOException
+     */
+    public static String doPost(String url, Map<String, String> params, String charset,
+                                int connectTimeout, int readTimeout) throws IOException {
+        String ctype = "application/x-www-form-urlencoded;charset=" + charset;
+        String content = buildQuery(params, charset);
+        return doPost(url, ctype, content, charset, connectTimeout, readTimeout);
+    }
+
+    /**
+     * 执行HTTP POST请求。
+     * 
+     * @param url 请求地址
+     * @param ctype 请求类型
+     * @param content 请求字节数组
+     * @return 响应字符串
+     * @throws IOException
+     */
+    public static String doPost(String url, String ctype, String content, String charset,
+                                int connectTimeout, int readTimeout) throws IOException {
+        HttpURLConnection conn = null;
+        OutputStream out = null;
+        String rsp = null;
+        try {
+            conn = getConnection(new URL(url), METHOD_POST, ctype);
+            conn.setConnectTimeout(connectTimeout);
+            conn.setReadTimeout(readTimeout);
+
+            out = conn.getOutputStream();
+            byte[] outputBytes = {};
+            if (content != null) {
+                outputBytes = content.getBytes(charset);
+            }
+            out.write(outputBytes);
+            rsp = getResponseAsString(conn);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+
+        return rsp;
+    }
+
+    /**
+     * 执行HTTP GET请求。
+     * 
+     * @param url 请求地址
+     * @param params 请求参数
+     * @return 响应字符串
+     * @throws IOException
+     */
+    public static String doGet(String url, Map<String, String> params) throws IOException {
+        return doGet(url, params, DEFAULT_CHARSET);
+    }
+
+    /**
+     * 执行HTTP GET请求。
+     * 
+     * @param url 请求地址
+     * @param params 请求参数
+     * @param charset 字符集，如UTF-8, GBK, GB2312
+     * @return 响应字符串
+     * @throws IOException
+     */
+    public static String doGet(String url, Map<String, String> params,
+                               String charset) throws IOException {
+        HttpURLConnection conn = null;
+        String rsp = null;
+        try {
+            String ctype = "application/x-www-form-urlencoded;charset=" + charset;
+            String query = buildQuery(params, charset);
+            conn = getConnection(buildGetUrl(url, query), METHOD_GET, ctype);
+            rsp = getResponseAsString(conn);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+        return rsp;
+    }
+
+    private static HttpURLConnection getConnection(URL url, String method,
+                                                   String ctype) throws IOException {
+        HttpURLConnection conn = null;
+        if ("https".equals(url.getProtocol())) {
+            HttpsURLConnection connHttps = (HttpsURLConnection) url.openConnection();
+            connHttps.setSSLSocketFactory(SOCKET_FACTORY);
+            connHttps.setHostnameVerifier(VERIFIER);
+            conn = connHttps;
+        } else {
+            conn = (HttpURLConnection) url.openConnection();
+        }
+
+        conn.setRequestMethod(method);
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Accept", "text/xml,text/javascript,text/html");
+        conn.setRequestProperty("User-Agent", "top-sdk-java");
+        conn.setRequestProperty("Content-Type", ctype);
+        return conn;
+    }
+
+    private static URL buildGetUrl(String strUrl, String query) throws IOException {
+        URL url = new URL(strUrl);
+        if (CommonToolkits.isEmptyContainNull(query)) {
+            return url;
+        }
+
+        if (CommonToolkits.isEmptyContainNull(url.getQuery())) {
+            if (strUrl.endsWith("?")) {
+                strUrl = strUrl + query;
+            } else {
+                strUrl = strUrl + "?" + query;
+            }
+        } else {
+            if (strUrl.endsWith("&")) {
+                strUrl = strUrl + query;
+            } else {
+                strUrl = strUrl + "&" + query;
+            }
+        }
+
+        return new URL(strUrl);
+    }
+
+    public static String buildQuery(Map<String, String> params, String charset) throws IOException {
+        if (params == null || params.isEmpty()) {
+            return null;
+        }
+
+        StringBuilder query = new StringBuilder();
+        Set<Entry<String, String>> entries = params.entrySet();
+        boolean hasParam = false;
+
+        for (Entry<String, String> entry : entries) {
+            String name = entry.getKey();
+            String value = entry.getValue();
+            // 忽略参数名或参数值为空的参数
+            if (CommonToolkits.isNotEmpty(name)) {
+                if (hasParam) {
+                    query.append("&");
+                } else {
+                    hasParam = true;
+                }
+
+                query.append(name).append("=").append(URLEncoder.encode(value, charset));
+            }
+        }
+
+        return query.toString();
+    }
+
+    private static String getResponseAsString(HttpURLConnection conn) throws IOException {
+        String charset = getResponseCharset(conn.getContentType());
+        InputStream es = conn.getErrorStream();
+        if (es == null) {
+            return getStreamAsString(conn.getInputStream(), charset);
+        } else {
+            String msg = getStreamAsString(es, charset);
+            if (CommonToolkits.isEmptyContainNull(msg)) {
+                throw new IOException(conn.getResponseCode() + ":" + conn.getResponseMessage());
+            } else {
+                throw new IOException(msg);
+            }
+        }
+    }
+
+    private static String getStreamAsString(InputStream stream, String charset) throws IOException {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(stream, charset));
+            StringWriter writer = new StringWriter();
+
+            char[] chars = new char[256];
+            int count = 0;
+            while ((count = reader.read(chars)) > 0) {
+                writer.write(chars, 0, count);
+            }
+
+            return writer.toString();
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+    }
+
+    private static String getResponseCharset(String ctype) {
+        String charset = DEFAULT_CHARSET;
+
+        if (CommonToolkits.isNotEmpty(ctype)) {
+            String[] params = ctype.split(";");
+            for (String param : params) {
+                param = param.trim();
+                if (param.startsWith("charset")) {
+                    String[] pair = param.split("=", 2);
+                    if (pair.length == 2) {
+                        if (CommonToolkits.isNotEmpty(pair[1])) {
+                            charset = pair[1].trim();
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        return charset;
+    }
+
+    /**
+     * 使用默认的UTF-8字符集反编码请求参数值。
+     * 
+     * @param value 参数值
+     * @return 反编码后的参数值
+     */
+    public static String decode(String value) {
+        return decode(value, DEFAULT_CHARSET);
+    }
+
+    /**
+     * 使用默认的UTF-8字符集编码请求参数值。
+     * 
+     * @param value 参数值
+     * @return 编码后的参数值
+     */
+    public static String encode(String value) {
+        return encode(value, DEFAULT_CHARSET);
+    }
+
+    /**
+     * 使用指定的字符集反编码请求参数值。
+     * 
+     * @param value 参数值
+     * @param charset 字符集
+     * @return 反编码后的参数值
+     */
+    public static String decode(String value, String charset) {
+        String result = null;
+        if (CommonToolkits.isNotEmpty(value)) {
+            try {
+                result = URLDecoder.decode(value, charset);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 使用指定的字符集编码请求参数值。
+     * 
+     * @param value 参数值
+     * @param charset 字符集
+     * @return 编码后的参数值
+     */
+    public static String encode(String value, String charset) {
+        String result = null;
+        if (CommonToolkits.isNotEmpty(value)) {
+            try {
+                result = URLEncoder.encode(value, charset);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
+    }
+
+	/**
+	 * 向指定URL发送GET方法的请求
+	 * @param url 发送请求的URL
+	 * @param jsonParams 请求参数(JSON字符串格式)
+	 * @return URL 所代表远程资源的响应结果
+	 */
+	public static String jsondGet(String url, String jsonParams) {
+		String result = "";
+		BufferedReader in = null;
+		try {
+			String urlNameString = url + "?" + jsonParams;
+			URL realUrl = new URL(urlNameString);
+			// 打开和URL之间的连接
+			URLConnection connection = realUrl.openConnection();
+			// 设置通用的请求属性
+			// connection.setRequestProperty("accept", "*/*");
+			// connection.setRequestProperty("connection", "Keep-Alive");
+			// connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			// 建立实际的连接
+			connection.connect();
+			// 获取所有响应头字段
+			Map<String, List<String>> map = connection.getHeaderFields();
+			// 遍历所有的响应头字段
+			for (String key : map.keySet()) {
+				System.out.println(key + "--->" + map.get(key));
+			}
+			// 定义 BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送GET请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输入流
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * 向指定 URL 发送POST方法的请求
+	 * @param url 发送请求的 URL
+	 * @param jsonParams 请求参数(JSON字符串格式)
+	 * @return 所代表远程资源的响应结果
+	 */
+	public static String jsonPost(String url, String jsonParams) {
+		PrintWriter out = null;
+		BufferedReader in = null;
+		String result = "";
+		try {
+			URL realUrl = new URL(url);
+			// 打开和URL之间的连接
+			URLConnection conn = realUrl.openConnection();
+			// 设置通用的请求属性
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			conn.setRequestProperty("Content-Type", "applicaton/json");
+			// 发送POST请求必须设置如下两行
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			// 获取URLConnection对象对应的输出流
+			out = new PrintWriter(conn.getOutputStream());
+			// 发送请求参数
+			out.print(jsonParams);
+			// flush输出流的缓冲
+			out.flush();
+			// 定义BufferedReader输入流来读取URL的响应
+			in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			System.out.println("发送 POST 请求出现异常！" + e);
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输出流、输入流
+		finally {
+			try {
+				if (out != null) {
+					out.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+}
