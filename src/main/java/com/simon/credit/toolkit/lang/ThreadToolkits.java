cb353e4d83c45d1b79235cba738a564995197b6d
xuziming <xuzimin_g@163.com>
2019-11-18 11:14:26 +0800

    增加时间类

--- src/main/java/com/simon/credit/toolkit/lang/ThreadToolkits.java
+++ src/main/java/com/simon/credit/toolkit/lang/ThreadToolkits.java
@@ -2,6 +2,10 @@ package com.simon.credit.toolkit.lang;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 线程工具类
+ * @author XUZIMING 2019-11-18
+ */
 public class ThreadToolkits {
 
 	public static final void sleep(long timeout, TimeUnit timeUnit) {
@@ -10,6 +14,25 @@ public class ThreadToolkits {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static final void sleep(Time time) {
+		sleep(time.getDuration(), time.getTimeUnit());
+	}
+
+	public static void main(String[] args) {
+		System.out.println("started.");
+//		ThreadToolkits.sleep(Time.ONE_SECONDS);
+//		ThreadToolkits.sleep(Time.TWO_SECONDS);
+//		ThreadToolkits.sleep(Time.THREE_SECONDS);
+//		ThreadToolkits.sleep(Time.FOUR_SECONDS);
+		ThreadToolkits.sleep(Time.FIVE_SECONDS);
+//		ThreadToolkits.sleep(Time.SIX_SECONDS);
+//		ThreadToolkits.sleep(Time.SEVEN_SECONDS);
+//		ThreadToolkits.sleep(Time.EIGHT_SECONDS);
+//		ThreadToolkits.sleep(Time.NINE_SECONDS);
+//		ThreadToolkits.sleep(Time.TEN_SECONDS);
+		System.out.println("finished.");
 	}
 
 }
---
+++ src/main/java/com/simon/credit/toolkit/lang/Time.java
@@ -0,0 +1,44 @@
+package com.simon.credit.toolkit.lang;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 时间类
+ * @author XUZIMING 2019-11-18
+ */
+public class Time {
+	/** 一秒 */public static final Time   ONE_SECONDS = Time.of( 1, TimeUnit.SECONDS);
+	/** 两秒 */public static final Time   TWO_SECONDS = Time.of( 2, TimeUnit.SECONDS);
+	/** 三秒 */public static final Time THREE_SECONDS = Time.of( 3, TimeUnit.SECONDS);
+	/** 四秒 */public static final Time  FOUR_SECONDS = Time.of( 4, TimeUnit.SECONDS);
+	/** 五秒 */public static final Time  FIVE_SECONDS = Time.of( 5, TimeUnit.SECONDS);
+	/** 六秒 */public static final Time   SIX_SECONDS = Time.of( 6, TimeUnit.SECONDS);
+	/** 七秒 */public static final Time SEVEN_SECONDS = Time.of( 7, TimeUnit.SECONDS);
+	/** 八秒 */public static final Time EIGHT_SECONDS = Time.of( 8, TimeUnit.SECONDS);
+	/** 九秒 */public static final Time  NINE_SECONDS = Time.of( 9, TimeUnit.SECONDS);
+	/** 十秒 */public static final Time  TEN_SECONDS  = Time.of(10, TimeUnit.SECONDS);
+
+	/** 持续时间 */
+	private long duration;
+
+	/** 时间单位 */
+	private TimeUnit timeUnit;
+
+	public Time(long duration, TimeUnit timeUnit) {
+		this.duration = duration;
+		this.timeUnit = timeUnit;
+	}
+
+	public static Time of(long duration, TimeUnit timeUnit) {
+		return new Time(duration, timeUnit);
+	}
+
+	public long getDuration() {
+		return duration;
+	}
+
+	public TimeUnit getTimeUnit() {
+		return timeUnit;
+	}
+
+}
