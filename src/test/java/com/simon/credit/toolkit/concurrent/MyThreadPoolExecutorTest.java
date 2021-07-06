cdf62df60195a9aa235dcbd00d03c2391e0ae20f
xuziming <xuzimin_g@163.com>
2021-07-06 10:52:06 +0800

    improve some codes

--- src/test/java/com/simon/credit/toolkit/concurrent/MyThreadPoolExecutorTest.java
+++ src/test/java/com/simon/credit/toolkit/concurrent/MyThreadPoolExecutorTest.java
@@ -5,48 +5,42 @@ import java.util.concurrent.TimeUnit;
 
 public class MyThreadPoolExecutorTest {
 
-	public static void main(String[] args) {
-		MyThreadPoolExecutor executor = null;
-		// BlockingThreadPool blockingThreadPool = null;
-		try {
-			// 创建线程池
-			executor = new MyThreadPoolExecutor(2, 5, 5, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(2));
-			// blockingThreadPool = BlockingThreadPool.newBlockingThreadPool(Runtime.getRuntime().availableProcessors());
+    public static void main(String[] args) {
+        MyThreadPoolExecutor executor = null;
+        try {
+            // 创建线程池
+            executor = new MyThreadPoolExecutor(2, 5, 5, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(2));
 
-			for (int i = 1; i <= 50; i++) {
-				Task task = new Task("task-" + i);
-				executor.execute(task);
-				// blockingThreadPool.execute(task);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (!executor.isShutdown()) {
-				executor.shutdown();
-			}
-//			if (!blockingThreadPool.isShutdown()) {
-//				blockingThreadPool.shutdown();
-//			}
-		}
-	}
+            for (int i = 1; i <= 50; i++) {
+                Task task = new Task("task-" + i);
+                executor.execute(task);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (!executor.isShutdown()) {
+                executor.shutdown();
+            }
+        }
+    }
 
 }
 
 class Task extends Thread {
 
-	private String taskName;
+    private String taskName;
 
-	Task(String taskName) {
-		this.taskName = taskName;
-	}
+    Task(String taskName) {
+        this.taskName = taskName;
+    }
 
-	public void run() {
-		try {
-			System.out.println(taskName + " is running...");
-			Thread.sleep(1000);// 线程睡眠一秒
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public void run() {
+        try {
+            System.out.println(taskName + " is running...");
+            Thread.sleep(1000);// 线程睡眠一秒
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
